9cd74d1653ded9ff7e960f829b3581529c95597d
Gandhali-18 <gandhalisanas588@gmail.com>
2025-07-12 23:08:10 +0530

    about edit

--- src/About.js
+++ src/About.js
@@ -1,3 +1,7 @@
+import cafe1 from "./daan-evers-tKN1WXrzQ3s-unsplash.jpg";
+import cafe2 from "./shawn-nmpW_WwwVSc-unsplash.jpg";
+import cafe3 from "./cafe3.jpeg";
+
 const About = () => {
     return ( <h1>Hello About</h1> );
 }
